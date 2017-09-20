b85f6051a9ef263e2b0bb67ffb06f7d88055e224
Sgathec <this-is-not-a-real-address@fakemail.com>
2017-09-20 17:20:30 +0300

    Assignment finished

--- src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
+++ src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
@@ -36,7 +36,130 @@ public class GildedRoseTest {
 	}
     
 	@Test
-	public void testUpdateEndOfDay() {
-		fail("Test not implemented");
+	public void testUpdateEndOfDay_itemQualityDecreasedTwiceAsFast() {
+		//Arrange
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("Reinforced Barrel", -1, 5));
+		
+		//Act
+		store.updateEndOfDay();
+		
+		//Assert
+		Item item = store.getItems().get(0);
+		assertEquals(3, item.getQuality());
 	}
+	
+	@Test
+	public void testUpdateEndOfDay_backstagePassQualityIncreasesBy2WhenSellinIs10orLess() {
+		//Arrange
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("Backstage passes to a TAFKAL80ETC concert", 10, 5));
+		
+		//Act
+		store.updateEndOfDay();
+		
+		//Assert
+		Item item = store.getItems().get(0);
+		assertEquals(7, item.getQuality());
+	}
+	
+	@Test
+	public void testUpdateEndOfDay_qualityIsNeverNegative() {
+		//Arrange
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("Ugly Hat", 30, 0));
+		
+		//Act
+		store.updateEndOfDay();
+		
+		//Assert
+		Item item = store.getItems().get(0);
+		assertEquals(0, item.getQuality());
+	}
+	
+	@Test
+	public void testUpdateEndOfDay_qualityIsNeverOver50() {
+		//Arrange
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("Aged Brie", 30, 50));
+		
+		//Act
+		store.updateEndOfDay();
+		
+		//Assert
+		Item item = store.getItems().get(0);
+		assertEquals(50, item.getQuality());
+	}
+	
+	
+	@Test
+	public void testUpdateEndOfDay_backstagePassQualityIncreasesBy3WhenSellinIs5orLess() {
+		//Arrange
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("Backstage passes to a TAFKAL80ETC concert", 5, 5));
+		
+		//Act
+		store.updateEndOfDay();
+		
+		//Assert
+		Item item = store.getItems().get(0);
+		assertEquals(8, item.getQuality());
+	}
+	
+	@Test
+	public void testUpdateEndOfDay_sulfurasDoesntLowerInQuality() {
+		//Arrange
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("Sulfuras, Hand of Ragnaros", 10, 80));
+		
+		//Act
+		store.updateEndOfDay();
+		
+		//Assert
+		Item item = store.getItems().get(0);
+		assertEquals(80, item.getQuality());
+	}
+	
+	@Test
+	public void testUpdateEndOfDay_sulfurasDoesntLowerInSellinValue() {
+		//Arrange
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("Sulfuras, Hand of Ragnaros", 10, 80));
+		
+		//Act
+		store.updateEndOfDay();
+		
+		//Assert
+		Item item = store.getItems().get(0);
+		assertEquals(10, item.getSellIn());
+	}
+	
+	@Test
+	public void testUpdateEndOfDay_itemQualityLoweredBy1() {
+		//Arrange
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("Colorful Hat", 20, 15));
+		
+		//Act
+		store.updateEndOfDay();
+		
+		//Assert
+		Item item = store.getItems().get(0);
+		assertEquals(14, item.getQuality());
+	}
+	
+	@Test
+	public void testUpdateEndOfDay_itemSellinLoweredBy1() {
+		//Arrange
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("Blue Shoes", 25, 5));
+		
+		//Act
+		store.updateEndOfDay();
+		
+		//Assert
+		Item item = store.getItems().get(0);
+		assertEquals(24, item.getSellIn());
+	}
+	
 }
